7e3ac401a84d6444bb1c0a1ddef74cbb7a5db2ac
Elena Christensen <olenachristensen777@gmail.com>
2022-08-19 09:18:31 +0200

    bug with login fixed

--- src/api/api.js
+++ src/api/api.js
@@ -34,20 +34,10 @@ export const usersAPI = {
   getProfile(userId) {
     return instance.get(`profile/` + userId)
       .then(response => {
+        
         return response.data;
       });
   },
-}
-
-export const login = () => {
-  return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-      withCredentials: true
-  }).then(response => {
-    debugger;
-      if (response.data.resultCode === 0) {
-        return response.data;
-      }
-    });
 }
 
 export const authAPI = {
