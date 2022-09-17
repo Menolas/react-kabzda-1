b5ab19957ddead0a05503355ed28a1d03ee14de6
Elena Christensen <olenachristensen777@gmail.com>
2022-09-17 09:59:47 +0200

    93 Lesson portions for pages in paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Paginator.module.css';
 
-let Paginator = (props) => {
+let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
   
-  let pagesCount = Math.ceil(props.totalItemsCount / props.pageSize);
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
   let pages = [];
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
 
-  let slicedPages = pages.slice(pagesCount - 6, pagesCount - 1);
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setPortionNumber] = useState(1);
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
+
+  let FilteredPages = pages
+    .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+    .map((p) => {
+      return (
+        <span
+          key={p}
+          className={currentPage === p ? classes.selected : ''}
+          onClick={(e) => { onPageChanged(p) }}>
+          {p}
+        </span>
+      )
+    });
 
   return (
     <div className={classes.pagination}>
-      {
-        slicedPages.map(
-          p => {
-            return (
-              <span
-                key={p}
-                className={props.currentPage === p ? classes.selected : ''}
-                onClick={(e) => {props.onPageChanged(p)}}>
-                {p}
-              </span>
-            )
-          })
-      }
+      { portionNumber > 1 &&
+        <button onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button>}
+      
+      {FilteredPages}
+
+      { portionCount > portionNumber &&
+        <button onClick={() => { setPortionNumber(portionNumber + 1) }}>NEXT</button> }
     </div>
   )
 };
